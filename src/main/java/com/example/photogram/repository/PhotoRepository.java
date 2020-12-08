ef4d3feff734c921bc6996f432e2d9804e5103a5
nan0machinesun <nan0machinesun@gmail.com>
2020-12-08 23:57:08 +0200

    Add new methods

--- src/main/java/com/example/photogram/repository/PhotoRepository.java
+++ src/main/java/com/example/photogram/repository/PhotoRepository.java
@@ -3,5 +3,8 @@ package com.example.photogram.repository;
 import com.example.photogram.model.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+    List<Photo> findPhotosByPostId(Long postId);
 }
--- src/main/java/com/example/photogram/repository/PostRepository.java
+++ src/main/java/com/example/photogram/repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.example.photogram.repository;
 import com.example.photogram.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findPostsByUserId(Long userId);
 }
